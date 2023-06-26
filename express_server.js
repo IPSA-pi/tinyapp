774ce026d898181a0798b408d1e612b1aa0bcacd
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-25 19:01:45 -0700

    Update expressserver.js to store short-url and long-url key value pair to database when a POST request is received to /urls

--- express_server.js
+++ express_server.js
@@ -14,8 +14,14 @@ const urlDatabase = {
 app.use(express.urlencoded({ extended: true}));
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
+})
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 })
 
 app.get("/", (req, res) => {
